9f682462217c2676a8ac4e77c6439b46ac53dc99
Uday <udaydevsani24@gmail.com>
2024-04-10 00:16:57 +0530

    minor changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect('mongodb+srv://udaydevsani24:Nh07EHvu6iK0x5pZ@cluster0.gtpw4xs.mongodb.net/crud', {
+mongoose.connect('mongodb://localhost:27017/crud', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
